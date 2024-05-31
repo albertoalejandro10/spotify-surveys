61859b64bac94231bc1fc10811d062b6ce11bdeb
AlbertoAlejandro <alberto196g@gmail.com>
2024-05-31 13:27:25 -0400

    Improved request to get the full list of Spotify tracks

--- src/api/getPlaylist.ts
+++ src/api/getPlaylist.ts
@@ -1,19 +1,48 @@
 import type { Playlist } from "../interfaces/Playlist";
 
-export const getPlaylist = async (playlist_id: string, token: string): Promise<Playlist> => {
-  const url = `https://api.spotify.com/v1/playlists/${playlist_id}`;
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const getPlaylist = async (playlistId: string, token: string): Promise<Playlist> => {
+  try {
+    const url = `https://api.spotify.com/v1/playlists/${playlistId}`;
+    let response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  if (!response.ok) {
-    throw new Error("Error al obtener la lista de playlist");
+    if (!response.ok) {
+      throw new Error(`HTTP error status: ${response.status}`);
+    }
+
+    const data: Playlist = await response.json();
+
+    // Fetching tracks recursively until there are no more pages
+    const fetchTracks = async () => {
+      const nextPageUrl = data.tracks.next;
+      if (!nextPageUrl) return;
+
+      response = await fetch(nextPageUrl, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error fetching tracks status: ${response.status}`);
+      }
+
+      const nextData = await response.json();
+      data.tracks.items.push(...nextData.items);
+      data.tracks.next = nextData.next;
+      await fetchTracks(); // Recursively call fetchTracks to handle pagination
+    };
+
+    await fetchTracks(); // Start fetching tracks
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error; // Rethrow the error to allow calling functions to handle it
   }
-
-  // Aquí es donde especificas el tipo de dato esperado
-  const data: Playlist = await response.json();
-  return data;
 };
